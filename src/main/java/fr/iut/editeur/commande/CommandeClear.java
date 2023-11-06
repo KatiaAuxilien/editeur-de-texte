ecd2bbc235e7c1c61de23da4c3cd7b86608e7fef
auxilienk <auxilien@etud.fr>
2023-11-06 21:22:46 +0100

    Ajout de la class clear et executer()
    Ajout de la commande à factory
    
    Ajout commandefactory
    
    Ajout condition

---
+++ src/main/java/fr/iut/editeur/commande/CommandeClear.java
@@ -0,0 +1,25 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeClear extends CommandeDocument {
+
+
+    public CommandeClear(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 2) {
+            System.err.println("Format attendu : effacer;");
+            return;
+        }
+        int indexDebut = 0;
+        int indexFin = this.document.getTexte().length();
+        String texte = "";
+        this.document.remplacer(indexDebut,indexFin,texte);
+        super.executer();
+    }
+
+}
--- src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
+++ src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
@@ -22,6 +22,7 @@ public class CommandeFactory {
             case "remplacer" : return new CommandeRemplacer(document, parameters);
             case "majuscules" : return new CommandeMajuscules(document, parameters);
             case "effacer" : return new CommandeEffacer(document,parameters);
+            case "clear" : return new CommandeClear(document,parameters);
             default: return null;
         }
     }
