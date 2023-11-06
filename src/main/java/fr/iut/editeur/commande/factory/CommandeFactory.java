5cadc066592335d56a9b53aeb8f199f0cfe3a6bc
auxilien <katia.auxilien@etu.umontpellier.fr>
2023-11-06 10:33:29 +0100

    ajout effacer

--- src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
+++ src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
@@ -21,6 +21,7 @@ public class CommandeFactory {
             case "ajouter" : return new CommandeAjouter(document, parameters);
             case "remplacer" : return new CommandeRemplacer(document, parameters);
             case "majuscules" : return new CommandeMajuscules(document, parameters);
+            case "effacer" : return new CommandeEffacer(document,parameters);
             default: return null;
         }
     }
