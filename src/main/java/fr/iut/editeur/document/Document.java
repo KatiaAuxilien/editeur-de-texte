3b189bd3d58fdce8a86943796935d08157e711ba
auxilienk <auxilien@etud.fr>
2023-11-13 18:30:52 +0100

    <doc> [TD2] : Ajout de la doc.

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -9,28 +9,57 @@ public class Document {
     public Document() {
         this.texteDocument = "";
     }
-	
+
+    /**
+     * Obtenir le texte contenu dans le document.
+     * @return texte string du document.
+     */
     public String getTexteDocument() {
         return texteDocument;
     }
 
+    /**
+     * Définir le texte contenu dans le document.
+     * @param texteDocument Texte de définition.
+     */
     public void setTexteDocument(String texteDocument) {
         this.texteDocument = texteDocument;
     }
 
+    /**
+     * Ajouter du texte à la suite.
+     * @param texte String texte à ajouter.
+     */
     public void ajouter(String texte) {
         this.texteDocument += texte;
     }
 
+    /**
+     * Effacer le contenu d'un point A à un point B (partant de 0).
+     * @param start en int point A
+     * @param end en int point B
+     */
     public void effacer(int start, int end){
         remplacer(start,end,"");
     }
+
+    /**
+     * Remplacer le contenu  d'un point A à un point B (partant de 0).
+     * @param start en int point A.
+     * @param end en int point B.
+     * @param remplacement String texte de remplacement.
+     */
     public void remplacer(int start, int end, String remplacement) {
         String leftPart = texteDocument.substring(0, start);
         String rightPart = texteDocument.substring(end);
         texteDocument = leftPart + remplacement + rightPart;
     }
 
+    /**
+     * Mettre en majuscules le texte d'un point A à un point B (partant de 0).
+     * @param start en int point A.
+     * @param end en int point B.
+     */
     public void majuscules(int start, int end) {
         String texte = getTexteDocument();
         String partieMaj = texte.substring(start,end);
@@ -38,6 +67,12 @@ public class Document {
         remplacer(start,end,partieMaj);
     }
 
+    /**
+     * Afficher la description de la commande précisée en argument.
+     * @param commande String commande dont on veut connaitre la description.
+     * @param parameters
+     * @param document
+     */
     public void description(String commande, String[] parameters,Document document){
         switch (commande) {
             case "ajouter" : (new CommandeAjouter(document, parameters)).getDescriptionCommande(); return;
@@ -51,6 +86,11 @@ public class Document {
         }
     }
 
+    /**
+     * Mettre en minuscules le texte d'un point A à un point B (partant de 0).
+     * @param start en int point A.
+     * @param end en int point B.
+     */
     public void minuscules(int start, int end) {
         String texte = getTexteDocument();
         String partieMaj = texte.substring(start,end);
