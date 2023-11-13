5b4b574f1c1de9943e80ea67c89dd61f95708af8
auxilien <katia.auxilien@etu.umontpellier.fr>
2023-11-13 12:32:39 +0100

    <test>[td2] : Rédaction des tests.

---
+++ src/test/java/fr/iut/editeur/document/DocumentTest.java
@@ -0,0 +1,38 @@
+package fr.iut.editeur.document;
+
+import junit.framework.TestCase;
+
+public class DocumentTest extends TestCase {
+
+    private Document docTest;
+    protected void setUp() throws Exception{
+        super.setUp();
+        docTest = new Document();
+        docTest.ajouter("abc");
+    }
+
+    public void testDocument(){
+        assertNotNull("L'instance n'est pas créée",docTest);
+    }
+    public void testGetText() throws Exception {
+       assertEquals("Le contenu est incorrect.","abc",docTest.getTexteDocument());
+    }
+    public void testSetText() throws Exception {
+        docTest.setTexteDocument("efgh");
+        assertEquals("Le contenu est incorrect.","efgh",docTest.getTexteDocument());
+    }
+
+    public void testEffacer() throws Exception{
+        docTest.effacer(0,1);
+        assertEquals("Le contenu effacé est incorrect","bc",docTest.getTexteDocument());
+    }
+
+    public void testAjouter() throws Exception{
+        docTest.ajouter("de");
+        assertEquals("Le contenu ajouté est incorrect.","abcde",docTest.getTexteDocument());
+    }
+
+
+
+
+}
