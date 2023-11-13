dacf707a468d5634f6e666263b678a7cd15d33cd
auxilien <katia.auxilien@etu.umontpellier.fr>
2023-11-13 11:49:04 +0100

    <doc>[td2] : Ajout de la javadoc à Document.java

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -35,9 +35,9 @@ public class Document {
     }
 
     /**
-     *
-     * @param start
-     * @param end
+     * Effacer le texte d'un point a à un point b.
+     * @param start position de début.
+     * @param end position de fin.
      */
     public void effacer(int start, int end){
         remplacer(start,end,"");
@@ -48,6 +48,11 @@ public class Document {
         texte = leftPart + remplacement + rightPart;
     }
 
+    /**
+     * Mettre le texte en majuscule d'un point A à un point B.
+     * @param start point A
+     * @param end point B
+     */
     public void majuscules(int start, int end) {
         String texte = getTexte();
         String partieMaj = texte.substring(start,end);
@@ -55,6 +60,10 @@ public class Document {
         remplacer(start,end,partieMaj);
     }
 
+    /**
+     *
+     * @return Le document sous forme de chaine de caractère ?
+     */
     @Override
     public String toString() {
         return this.texte;
